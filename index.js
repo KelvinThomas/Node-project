d162810dc041febebc51bce3016ad5952827b592
Kelvin Thomas <kelvinthomas1801@gmail.com>
2024-08-16 10:21:04 +0300

    yoyo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-//import all the functions from math.js
-const { add, substract, multiply, divide, fact } = require("./math");
+// //import all the functions from math.js
+// const { add, substract, multiply, divide, fact } = require("./math");
 
-console.log("Add: ", add(3, 7));
-console.log("substract: ", substract(98,87));
-console.log("multiply: ", multiply(86,762));
-console.log("Divide: ", divide(7386,46));
+// console.log("Add: ", add(3, 7));
+// console.log("substract: ", substract(98,87));
+// console.log("multiply: ", multiply(86,762));
+// console.log("Divide: ", divide(7386,46));
 
-console.log("Factorial", fact(5));
+// console.log("Factorial", fact(5));
 
 
 
@@ -22,3 +22,20 @@ console.log("Factorial", fact(5));
 //   console.log(data);
 // });
 // console.log("File reading started...");
+
+
+// app.js
+const fs = require('fs');
+
+// Replace 'example.txt' with the path to your file
+const filePath = 'example.txt';
+
+// Asynchronously read the file
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+    console.log('File content:');
+    console.log(data);
+});
